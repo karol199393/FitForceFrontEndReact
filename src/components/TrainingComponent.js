cfe41b5236a2616902591daca99ebbb0e5bd562a
Karol <114170012+karol199393@users.noreply.github.com>
2024-05-05 20:02:00 +0200

    Add assign exercise to training

--- src/components/TrainingComponent.js
+++ src/components/TrainingComponent.js
@@ -4,6 +4,8 @@ import './training.css';
 
 const TrainingComponent = () => {
     const [trainings, setTrainings] = useState([]);
+    const [exercises, setExercises] = useState([]);
+    const [selectedExercise, setSelectedExercise] = useState(null);
 
     useEffect(() => {
         axios.get('http://localhost:8080/api/v1/trainings')
@@ -15,6 +17,27 @@ const TrainingComponent = () => {
             });
     }, []);
 
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/v1/exercises')
+            .then(response => {
+                setExercises(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    const assignExerciseToTraining = (exerciseId, trainingId) => {
+        fetch(`http://localhost:8080/api/v1/exercises/${exerciseId}/assign/${trainingId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json())
+        .then(data => console.log('Success:', data))
+        .catch(error => console.error('Error:', error));
+    };
     return (
         <div>
             <h1>Treningi</h1>
@@ -23,6 +46,7 @@ const TrainingComponent = () => {
                     <tr>
                         <th>Id</th>
                         <th>Nazwa Treningu</th>
+                        <th>Przypisz Ćwiczenie</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -30,6 +54,15 @@ const TrainingComponent = () => {
                         <tr key={training.id}>
                             <td>{training.id}</td>
                             <td>{training.name}</td>
+                            <td>
+                                <select onChange={(event) => setSelectedExercise(event.target.value)}>
+                                    <option value="">Wybierz ćwiczenie</option>
+                                    {exercises.map(exercise => (
+                                        <option key={exercise.id} value={exercise.id}>{exercise.name}</option>
+                                    ))}
+                                </select>
+                                <button onClick={() => assignExerciseToTraining(selectedExercise, training.id)}>Przypisz</button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
